21dfc43df95467808c536f586a468db6c5404bd0
Andrey Bliznev <vto.site@mail.ru>
2021-11-12 14:44:39 +0300

    add sticky footer

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
 import "bootstrap/dist/css/bootstrap-reboot.css"
 import "bootstrap/dist/css/bootstrap-utilities.css"
 import "bootstrap/dist/css/bootstrap-grid.css"
+import "./src/fonts/google-fonts.css"
+import "./src/components/sticky-footer-helper.css"
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+
+const Content = styled.div`
+  flex: 1 0 auto;
+`;
+
+const Header = styled.header`
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Footer = styled.footer`
+  flex-shrink: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const Main = styled.main``;
 
 const Layout = ({ children }) => {
   return (
-    <div className="container">
-      <div>Layout</div> {children}
-    </div>
+    <>
+      <Content>
+        <Header>
+          <h1>Header</h1>
+        </Header>
+        <Main className="container">{children}</Main>
+      </Content>
+      <Footer>
+        <h1>Footer</h1>
+      </Footer>
+    </>
   );
 };
 
