be15f16857806037ca911faa8b98a5671af62ac2
Andrey Bliznev <vto.site@mail.ru>
2021-11-15 12:28:52 +0300

    blockout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,25 +5,16 @@ const Content = styled.div`
   flex: 1 0 auto;
 `;
 
-const Header = styled.header`
-  background-color: rgba(0, 0, 0, 0.5);
-`;
-
 const Footer = styled.footer`
   flex-shrink: 0;
   background-color: rgba(0, 0, 0, 0.5);
 `;
 
-const Main = styled.main``;
-
 const Layout = ({ children }) => {
   return (
     <>
       <Content>
-        <Header>
-          <h1>Header</h1>
-        </Header>
-        <Main className="container">{children}</Main>
+        {children}
       </Content>
       <Footer>
         <h1>Footer</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,17 +6,15 @@ import ProductCard from "../components/product/card";
 
 const Main = styled.main`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: stretch;
+  align-items: stretch;
   flex-direction: column;
-  padding: 25px;
 `;
 
 const IndexPage = () => {
   return (
     <Layout>
       <Main>
-        <h1>Index</h1>
         <ProductCard />
       </Main>
     </Layout>
