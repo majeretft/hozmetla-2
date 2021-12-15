49dd84559354edef5c02bbaaaf92e9ac7b1505f4
Andrey Bliznev <vto.site@mail.ru>
2021-12-15 18:08:11 +0300

    draft

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,17 +1,43 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useEffect, useRef } from "react";
+// import styled from "styled-components";
 
 import Layout from "../components/layout";
 import Navbar from "../components/layout/navbar";
 
 const Page = () => {
+  const mapContainer = useRef(null);
+
+  useEffect(() => {
+    const el = mapContainer.current;
+
+    const script = document.createElement("script");
+
+    script.src =
+      "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A_arpLO4PE819A1YjFE9m3U46OvMaJ1Kv&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true";
+    script.async = true;
+    script.type = "text/javascript";
+
+    el.appendChild(script);
+
+    return () => {
+        el.removeChild(script);
+    };
+  }, []);
+
   return (
     <Layout>
       <header>
         <Navbar />
       </header>
-      <main>
-          <h1>Contact</h1>
+      <main className="container">
+        <h1>Контакты</h1>
+        <p>+7 4912 50 643</p>
+        <p>sales@hozmetla.ru</p>
+        <p>
+          390017 Россия, Рязанская область, Рязань, Ряжское шоссе 20 литер Щ'
+        </p>
+        <p>Офис: 8:00-17:00</p>
+        <div ref={mapContainer}></div>
       </main>
     </Layout>
   );
--- src/templates/product.jsx
+++ src/templates/product.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 import Layout from "../components/layout";
 import Navbar from "../components/layout/navbar";
 
-const IndexPage = () => {
+const Page = () => {
   return (
     <Layout>
       <header>
         <Navbar />
       </header>
       <main className="container">
-        <h1>About</h1>
+        <h1>Product</h1>
       </main>
     </Layout>
   );
 };
 
-export default IndexPage;
+export default Page;
